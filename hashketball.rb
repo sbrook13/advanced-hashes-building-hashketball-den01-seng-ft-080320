3648842c21d299c17a737e126b1cb3e15bde75cf
Shelley Brook <sbrook13@gmail.com>
2020-07-19 21:38:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,4 +9,28 @@
 #       :shoe
 #       :points
 #       :rebounds
-#
+#       :assists
+#       :steals
+#       :blocks
+#       :slam_dunks
+
+def game_hash
+  :home => {
+    :team_name => "Brooklyn Nets",
+    :colors => [
+      "Black",
+      "White"
+      ],
+    :players => [
+      "Alan Anerson" =>{
+        :player_name => "Alan Anerson"
+        :number => 0 
+         :shoe
+#       :points
+#       :rebounds
+#       :assists
+#       :steals
+#       :blocks
+#       :slam_dunks
+      }]  
+  }
