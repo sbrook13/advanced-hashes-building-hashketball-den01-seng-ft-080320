5979430841fd58188cd993dfc1b13febaae84819
Shelley Brook <sbrook13@gmail.com>
2020-07-19 21:19:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,12 @@
 # Write your code here!
+#:home
+#:away
+#   :team_name - "string"
+#   :colors - array of strings
+#   :players - array of hashes 
+#       :player_name
+#       :number
+#       :shoe
+#       :points
+#       :rebounds
+#
