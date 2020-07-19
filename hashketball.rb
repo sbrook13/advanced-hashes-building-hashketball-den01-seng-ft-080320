fb4d91d5c80cc33dcfd2e1b95a003f5490c435e8
Shelley Brook <sbrook13@gmail.com>
2020-07-19 21:50:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -74,7 +74,7 @@ def game_hash
       },           
       ]  
   },
-    :away => {
+  :away => {
     :team_name => "Charlotte Hornets",
     :colors => [
       "Turquoise",
@@ -89,7 +89,9 @@ def game_hash
         :assists => 1,
         :steals => 2, 
         :blocks => 7,
-        :slam_dunks => 2},
+        :slam_dunks => 2
+        
+      },
       { :player_name => "Bismack Biyombo",
         :number => 0,
         :shoe => 16,
